d7954e82bc4419bbada99c5528e87b650317dce4
Himmat Singh <himmat.sidhu521@gmail.com>
2020-11-03 15:44:43 -0800

    Initial Commit

---
+++ Color-magic-protocols/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Color-magic-protocols
+//
+//  Created by Himmat Singh on 11/3/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
